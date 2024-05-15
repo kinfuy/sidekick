bb2bcf3f740a3cc8bb1be7fdda8f3a33118357ed
kinfuy <Yonghu520@outlook.com>
2024-05-15 21:08:09 +0800

    fix: detail

--- packages/server/src/dto/login.dto.ts
+++ packages/server/src/dto/login.dto.ts
@@ -1,15 +1,23 @@
+import { IsString } from 'class-validator';
 export class LoginDto {
+    @IsString()
     email: string;
+    @IsString()
     password: string;
 }
 
 export class RegisterDto {
+    @IsString()
     userName: string;
+    @IsString()
     email: string;
+    @IsString()
     password: string;
+    @IsString()
     verifyCode:string;
 }
 
 export class VerifyCodeDto {
+    @IsString()
     email: string;
 }
