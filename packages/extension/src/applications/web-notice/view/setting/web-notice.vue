04767ff40f173b90ac7de567bfbef49b070dd156
kinfuy <yonghu520@outlook.com>
2024-07-11 23:20:44 +0800

    fix: detail

--- packages/extension/src/applications/web-notice/view/setting/web-notice.vue
+++ packages/extension/src/applications/web-notice/view/setting/web-notice.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="web-notice">
+  <div v-if="whiteList.length" class="web-notice">
     <ElTag
       v-for="item in whiteList"
       :key="item.url"
@@ -10,10 +10,11 @@
       {{ item.url }}
     </ElTag>
   </div>
+  <ElEmpty v-else description="暂无数据" />
 </template>
 
 <script lang="ts" setup>
-import { ElTag } from 'element-plus';
+import { ElEmpty, ElTag } from 'element-plus';
 import { useWebNoticeStore } from '../../store';
 
 const { whiteList, removeWeb } = useWebNoticeStore();
