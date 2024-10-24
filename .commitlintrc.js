587ad91ceb571838ea1846ad1253d242a193a86f
kinfuy <kinfuy@outlook.com>
2024-10-24 18:04:07 +0800

    fix: detail

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
-    'type-enum': [2, 'always', ['build', 'ci', 'chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
+    'type-enum': [2, 'always', ['build','release', 'ci', 'chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
   },
 };
